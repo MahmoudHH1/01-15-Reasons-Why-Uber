fa41c8ebaad419a5dc66114de16c30f49481f842
Youssef Maged <youssef.maged.6000@gmail.com>
2026-03-26 00:52:05 +0200

    feat(location-service): add transactional purge old location data endpoint with tests (55-2829)

---
+++ location-service/src/main/java/com/team01/uber/location/dto/PurgeResponse.java
@@ -0,0 +1,15 @@
+package com.team01.uber.location.dto;
+
+public class PurgeResponse {
+
+    private final long deletedCount;
+
+    public PurgeResponse(long deletedCount) {
+        this.deletedCount = deletedCount;
+    }
+
+    public long getDeletedCount() {
+        return deletedCount;
+    }
+}
+
