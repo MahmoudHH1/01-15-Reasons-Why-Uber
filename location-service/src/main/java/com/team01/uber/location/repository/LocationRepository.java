3b86a2741f990b6dafea5302d884a89c7681dfe0
OHarridy <oelharridy@gmail.com>
2026-03-26 14:21:41 +0200

    fix(location-service): add missing imports (55-0654)

--- location-service/src/main/java/com/team01/uber/location/repository/LocationRepository.java
+++ location-service/src/main/java/com/team01/uber/location/repository/LocationRepository.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import com.team01.uber.location.model.Location;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
