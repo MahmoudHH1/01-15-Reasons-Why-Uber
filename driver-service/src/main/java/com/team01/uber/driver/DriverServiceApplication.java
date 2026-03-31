f94aabf0b417a55c27a45da8832c65e552e55be6
MahmoudHH1 <mahmoudhanyhebishy@gmail.com>
2026-03-31 23:08:22 +0200

    feat(driver-service): configure RestTemplate bean and user-service URL (55-18387)

--- driver-service/src/main/java/com/team01/uber/driver/DriverServiceApplication.java
+++ driver-service/src/main/java/com/team01/uber/driver/DriverServiceApplication.java
@@ -2,6 +2,8 @@ package com.team01.uber.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DriverServiceApplication {
@@ -10,4 +12,8 @@ public class DriverServiceApplication {
         SpringApplication.run(DriverServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
