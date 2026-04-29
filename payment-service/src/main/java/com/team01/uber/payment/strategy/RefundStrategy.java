6685755b6bbdbdd5616f82424782e76ff96ca94c
seiftarek158 <seiftarek2424@gmail.com>
2026-04-30 00:42:07 +0300

    refactor(payment-service): RefundStrategy now returns RefundResult per spec (DP-1) (55-24853)

--- payment-service/src/main/java/com/team01/uber/payment/strategy/RefundStrategy.java
+++ payment-service/src/main/java/com/team01/uber/payment/strategy/RefundStrategy.java
@@ -4,5 +4,5 @@ import com.team01.uber.payment.dto.RefundSurgeRequest;
 import com.team01.uber.payment.model.Payment;
 
 public interface RefundStrategy {
-    Payment execute(Payment payment, RefundSurgeRequest request, RefundContext ctx);
+    RefundResult calculateRefund(Payment payment, RefundSurgeRequest request);
 }
