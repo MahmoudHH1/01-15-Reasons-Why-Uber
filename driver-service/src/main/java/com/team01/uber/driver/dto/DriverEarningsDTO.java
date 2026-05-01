b3b63e9d6a0b3d8f52e62ba709a6dd9e57d73a47
ahmed20150 <ahmed1.gasser@gmail.com>
2026-05-01 23:18:10 +0300

    feat(driver-service): add DP-4 Builder to DriverEarningsDTO S2-F3 (55-25085)

--- driver-service/src/main/java/com/team01/uber/driver/dto/DriverEarningsDTO.java
+++ driver-service/src/main/java/com/team01/uber/driver/dto/DriverEarningsDTO.java
@@ -1,5 +1,14 @@
 package com.team01.uber.driver.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DriverEarningsDTO {
 
     private Long driverId;
@@ -7,54 +16,4 @@ public class DriverEarningsDTO {
     private Long totalRides;
     private Double totalEarnings;
     private Double averageFare;
-
-    public DriverEarningsDTO() {}
-
-    public DriverEarningsDTO(Long driverId, String name, Long totalRides, Double totalEarnings, Double averageFare) {
-        this.driverId = driverId;
-        this.name = name;
-        this.totalRides = totalRides;
-        this.totalEarnings = totalEarnings;
-        this.averageFare = averageFare;
-    }
-
-    public Long getDriverId() {
-        return driverId;
-    }
-
-    public void setDriverId(Long driverId) {
-        this.driverId = driverId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getTotalRides() {
-        return totalRides;
-    }
-
-    public void setTotalRides(Long totalRides) {
-        this.totalRides = totalRides;
-    }
-
-    public Double getTotalEarnings() {
-        return totalEarnings;
-    }
-
-    public void setTotalEarnings(Double totalEarnings) {
-        this.totalEarnings = totalEarnings;
-    }
-
-    public Double getAverageFare() {
-        return averageFare;
-    }
-
-    public void setAverageFare(Double averageFare) {
-        this.averageFare = averageFare;
-    }
 }
