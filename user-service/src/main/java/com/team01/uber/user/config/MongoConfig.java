9dda20c47048d4d51eb3fe8af56fed5fc8599b2e
seifabdalla <seif1281@gmail.com>
2026-05-01 03:15:37 +0300

    feat(user): add MongoConfig for MongoDB connection and log LOGGED_IN event via Observer (55-3258)

---
+++ user-service/src/main/java/com/team01/uber/user/config/MongoConfig.java
@@ -0,0 +1,44 @@
+package com.team01.uber.user.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database:ubermongo}")
+    private String database;
+
+    @Bean
+    @Primary
+    public MongoClient mongoClient() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(mongoUri))
+                .build();
+        return MongoClients.create(settings);
+    }
+
+    @Bean
+    @Primary
+    public MongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
+        return new SimpleMongoClientDatabaseFactory(mongoClient, database);
+    }
+
+    @Bean
+    @Primary
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTemplate(mongoDatabaseFactory);
+    }
+}
