b92984d5a67c051810d816d39de10e8cdebb1f08
LUKITA1416171822 <132844508+LUKITA1416171822@users.noreply.github.com>
2026-05-11 04:12:16 +0300

    fix(payment-service): scope EnableFeignClients to 3 needed clients, update Dockerfile for multi-module build (55-25376)

--- payment-service/src/main/java/com/team01/uber/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/team01/uber/payment/PaymentServiceApplication.java
@@ -3,11 +3,14 @@ package com.team01.uber.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import com.team01.uber.contracts.feign.DriverServiceClient;
+import com.team01.uber.contracts.feign.RideServiceClient;
+import com.team01.uber.contracts.feign.UserServiceClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCaching
-@EnableFeignClients(basePackages = "com.team01.uber.contracts.feign")
+@EnableFeignClients(clients = {UserServiceClient.class, RideServiceClient.class, DriverServiceClient.class})
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
