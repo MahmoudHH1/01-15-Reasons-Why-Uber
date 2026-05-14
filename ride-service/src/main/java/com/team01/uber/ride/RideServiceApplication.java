d1271b62c76fdc2519928a4a137dea8c4a26864f
Ahmed Wael <138620451+sp4rkii@users.noreply.github.com>
2026-05-14 20:45:18 +0300

    feat(contracts): added logback-spring.sql (55-13512)

--- ride-service/src/main/java/com/team01/uber/ride/RideServiceApplication.java
+++ ride-service/src/main/java/com/team01/uber/ride/RideServiceApplication.java
@@ -7,7 +7,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCaching
-@EnableFeignClients
+@EnableFeignClients(clients = {
+    com.team01.uber.contracts.feign.DriverServiceClient.class,
+    com.team01.uber.contracts.feign.LocationServiceClient.class,
+    com.team01.uber.contracts.feign.UserServiceClient.class
+})
 public class RideServiceApplication {
 
     public static void main(String[] args) {
